8157a69cb4675efa7de20b8d442035526165ccdb
Pradeep Saravanan <pradeep.saravanan@mallow-tech.com>
2022-12-09 18:31:16 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+const {NextFederationPlugin} = require('@module-federation/nextjs-mf')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
+    },
+    webpack: (config, options) => {
+        const {isServer} = options;
+        config.experiments = {topLevelAwait: true, layers: true};
+        config.plugins.push(
+            new NextFederationPlugin({
+                name: 'child',
+                remotes: {
+                    container: `container@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+                },
+                filename: 'static/chunks/remoteEntry.js',
+                exposes: {
+                  './catalog': './components/Catalog.js'
+                },
+                extraOptions: {
+                    exposePages: true
+                }
+            })
+        );
+        return config;
+    }
 }
 
 module.exports = nextConfig
