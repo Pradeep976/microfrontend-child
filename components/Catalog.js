7e12b0fde060310311e1e211af8dc735d8a3174d
Pradeep Saravanan <pradeep.saravanan@mallow-tech.com>
2022-12-09 20:50:33 +0530

    Simplified Catalog Component

--- components/Catalog.js
+++ components/Catalog.js
@@ -7,18 +7,18 @@ export const Catalog = () => {
     const [list, setList] = React.useState([]);
 
     const fetchData = async () => {
-        try{
-            const response = await fetch("https://jsonplaceholder.typicode.com/albums")
-            const data = await response.json();
-            console.log(data);
-            setList(data);
-        }catch (err) {
-            console.error(err)
-        }
+        // try{
+        //     const response = await fetch("https://jsonplaceholder.typicode.com/albums")
+        //     const data = await response.json();
+        //     console.log(data);
+        //     setList(data);
+        // }catch (err) {
+        //     console.error(err)
+        // }
     }
 
     React.useEffect(() => {
-        fetchData()
+        // fetchData()
     }, [])
 
     return <div>
