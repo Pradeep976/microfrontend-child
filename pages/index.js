2d0d205c03e742c407f946f5c34443f157b2157b
Pradeep Saravanan <pradeep.saravanan@mallow-tech.com>
2022-12-12 13:41:37 +0530

    simplified index js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import dynamic from 'next/dynamic'
+
+const Nav = dynamic(() => import('container.js/nav'));
+const Footer = dynamic(() => import('container.js/footer'));
+
 export default function Home() {
     return (
         <div>
